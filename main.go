728eedebd1270f27917b660ccb1dedae32ad8406
Christopher Hernandez <psychosisjr@gmail.com>
2026-07-13 19:58:03 -0400

    REPL L4: A working lesson 4 solution written using only vscode documentation (passing solution). Refinement soon.

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 import (
+	"os"
+	"bufio"
 	"fmt"
-	//"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	//arr := cleanInput("Hello World")
+
+	// scanner.Scan blocks then waits for ENTER
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for ;; {
+		fmt.Print("Pokedex > ")
+		scannerBool := scanner.Scan()
+
+		if scannerBool == true {
+			userInput := scanner.Text()
+			cleanedUserInput := CleanInput(userInput)
+			fmt.Printf("Your command was: %s\n", cleanedUserInput[0])
+		}
+
+		if scannerBool == false {
+			
+			fmt.Println("END OF FILE.")
+			err := scanner.Err()
+			if err == nil {
+				break
+			} else {
+				fmt.Println("Error.")
+				break
+			}
+
+		}
+		
+
+	}
+	
 	
 
 }
