3eb1d47ed9bf384076f12a4202b5d387e8ccd3dd
Christopher Hernandez <psychosisjr@gmail.com>
2026-07-14 19:33:35 -0400

    Fixed locations.go Location struct to reflect the JSON object. NamedAPIResource includes a Name and a URL which is a common pattern.

--- locations.go
+++ locations.go
@@ -1,11 +1,24 @@
-type Location struct {
-	Id     string     `json:"id"`
-	Name   string     `json:"name"`
-	Region struct {
-		Name     string     `json:"name"`
-		URL      string     `json:"url"`
-	} `json:"region"`
-	Names []string {
+package main
 
-	}
+type NamedAPIResource struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Location struct {
+	ID     int              `json:"id"`
+	Name   string           `json:"name"`
+	Region NamedAPIResource `json:"region"`
+
+	Names []struct {
+		Name     string           `json:"name"`
+		Language NamedAPIResource `json:"language"`
+	} `json:"names"`
+
+	GameIndices []struct {
+		GameIndex  int              `json:"game_index"`
+		Generation NamedAPIResource `json:"generation"`
+	} `json:"game_indices"`
+
+	Areas []NamedAPIResource `json:"areas"`
 }
